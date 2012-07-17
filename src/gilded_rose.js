986a7efe21e8c86f1ab639155b17c450d0ff4959
Amber Conville <crebma@gmail.com>
2012-07-17 18:43:48 -0400

    Extracting name accessor to local variable

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -18,17 +18,18 @@ items.push(new Item('Conjured Mana Cake', 3, 6));
 function update_quality() {
   for (var i = 0; i < items.length; i++) {
     var item = items[i];
+    var name = item.name;
 
-    if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+    if (name != 'Aged Brie' && name != 'Backstage passes to a TAFKAL80ETC concert') {
       if (item.quality > 0) {
-        if (item.name != 'Sulfuras, Hand of Ragnaros') {
+        if (name != 'Sulfuras, Hand of Ragnaros') {
           item.quality = item.quality - 1
         }
       }
     } else {
       if (item.quality < 50) {
         item.quality = item.quality + 1
-        if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
+        if (name == 'Backstage passes to a TAFKAL80ETC concert') {
           if (item.sell_in < 11) {
             if (item.quality < 50) {
               item.quality = item.quality + 1
@@ -42,14 +43,14 @@ function update_quality() {
         }
       }
     }
-    if (item.name != 'Sulfuras, Hand of Ragnaros') {
+    if (name != 'Sulfuras, Hand of Ragnaros') {
       item.sell_in = item.sell_in - 1;
     }
     if (item.sell_in < 0) {
-      if (item.name != 'Aged Brie') {
-        if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+      if (name != 'Aged Brie') {
+        if (name != 'Backstage passes to a TAFKAL80ETC concert') {
           if (item.quality > 0) {
-            if (item.name != 'Sulfuras, Hand of Ragnaros') {
+            if (name != 'Sulfuras, Hand of Ragnaros') {
               item.quality = item.quality - 1
             }
           }
